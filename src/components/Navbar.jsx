229326976bef7d35d15eb0ba7e838a24f9112f80
Martin <martingauna2000@gmail.com>
2022-01-18 22:31:11 -0300

    agregando logo y search al navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Search } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
@@ -7,12 +8,54 @@ const Container = styled.div`
 `;
 const Wrapper = styled.div`
     padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+const Input = styled.input`
+    border: none;
+`;
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+const Logo = styled.h1`
+    font-wight: bold;
+`;
+const Right = styled.div`
+    flex: 1;
 `;
 
 function Navbar() {
     return (
         <Container>
-           <Wrapper>Navbar</Wrapper> 
+           <Wrapper>
+               <Left>
+                   <Language>ES</Language>
+                   <SearchContainer>
+                       <Input /> 
+                       <Search />
+                   </SearchContainer>
+               </Left>
+               <Center><Logo>Hades</Logo></Center>
+               <Right>right</Right>
+           </Wrapper> 
         </Container>
     )
 }
