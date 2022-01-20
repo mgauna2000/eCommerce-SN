5b75352d29f67bda8eadf6a975cbd183c08a9c0b
Martin <martingauna2000@gmail.com>
2022-01-19 21:07:52 -0300

    añadiendo arrow al slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
         <div>
             <Announcement />
             <Navbar />
+            <Slider />
         </div>
     )
 }
