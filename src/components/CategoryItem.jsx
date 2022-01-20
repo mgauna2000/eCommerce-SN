3a3e769f9cf40a4f20af7bc4e4e4bcfa00444dbd
Martin <martingauna2000@gmail.com>
2022-01-20 20:11:58 -0300

    finalizando categories

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,13 +1,49 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-
+    position: relative;
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+`;
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
+const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+const Title = styled.h1`
+    color: white;
+    margin-bottom: 20px;
+`;
+const Button = styled.button`
+    border: none;
+    padding: 10px;
+    background-color: white;
+    color: gray;
+    cursor: pointer;
+    text-transform: uppercase;
+    font-weight: 600;
 `;
 
-const CategoryItem = (item) => {
+const CategoryItem = ({item}) => {
   return (
       <Container>
-          Hola
+          <Image src={item.img} />
+          <Info>
+              <Title>{item.title}</Title>
+              <Button>Compra ahora</Button>
+          </Info>
       </Container>
   )
 };
