e628479e7dff0ef2c33e18c3862bb8774ccc1f8e
Martin <martingauna2000@gmail.com>
2022-01-19 22:46:41 -0300

    slide terminado con items exportados

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images-na.ssl-images-amazon.com/images/I/91Fy3s2KyBL.jpg",
+        title: "Manga SNK",
+        desc: "Historia donde cuenta como Eren da vergazos a mucha gente",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://www.ubuy.om/productimg/?image=aHR0cHM6Ly9pbWFnZXMtbmEuc3NsLWltYWdlcy1hbWF6b24uY29tL2ltYWdlcy9JLzUxSUtLUXlmYmxMLl9TUzQwMF8uanBn.jpg",
+        title: "Tokio Ghoul",
+        desc: "Historia donde Kaneki folla con Touka-chan al final",
+        bg: "f5fafd",
+    },
+    {
+        id: 3,
+        img: "https://http2.mlstatic.com/D_NQ_NP_780153-MLA46604101698_072021-O.webp",
+        title: "Akame Ga Kill",
+        desc: "Un buen anime que me encanta su Waifu de pelo azul uwu",
+        bg: "f5fafd",
+    },
+];
