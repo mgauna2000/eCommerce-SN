38b8012a94283300f38a5fdcd19efb8fbbb4934b
Martin <martingauna2000@gmail.com>
2022-01-19 21:31:41 -0300

    añadiendo img y desc al slide

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -24,6 +24,40 @@ const Arrow = styled.div`
     cursor: pointer;
     opacity: 0.5;
 `;
+const Wrapper = styled.div`
+    height: 100%;
+`;
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh; 
+    display: flex;
+    align-items: center;
+`;
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`;
+const Image = styled.img`
+    height: 80%;
+`;
+const InfoContainer = styled.div`
+    flex: 1;
+`;
+const Title = styled.h1`
+    font-size: 70px;
+`;
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`;
 
 const Slider = () => {
   return (
@@ -31,6 +65,18 @@ const Slider = () => {
           <Arrow direction="left">
               <ArrowLeftOutlined/>
           </Arrow>
+          <Wrapper>
+            <Slide>
+            <ImgContainer>
+            <Image src="https://images-na.ssl-images-amazon.com/images/I/91Fy3s2KyBL.jpg" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Manga SNK</Title>
+                <Desc>Historia donde cuenta como Eren da vergazos a mucha gente</Desc>
+                <Button>Comprar</Button>
+            </InfoContainer>
+            </Slide>
+          </Wrapper>
           <Arrow direction="right">
               <ArrowRightOutlined/>
           </Arrow>
