c8181c371b4242bdd883e56860228286479597fd
Martin <martingauna2000@gmail.com>
2022-01-20 19:36:10 -0300

    añadiendo categories

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+
+`;
+
+const CategoryItem = (item) => {
+  return (
+      <Container>
+          Hola
+      </Container>
+  )
+};
+
+export default CategoryItem;
