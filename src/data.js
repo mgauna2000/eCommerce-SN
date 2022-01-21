2c6aec874ea38daab6030116a5010c24d6ca98b1
Martin <martingauna2000@gmail.com>
2022-01-20 22:19:15 -0300

    añadiendo product

--- src/data.js
+++ src/data.js
@@ -4,21 +4,21 @@ export const sliderItems = [
         img: "https://http2.mlstatic.com/D_NQ_NP_746797-MLA46526260507_062021-O.webp",
         title: "Manga SNK",
         desc: "Historia donde cuenta como Eren da vergazos a mucha gente",
-        bg: "fff",
+        bg: "CAE4E2",
     },
     {
         id: 2,
         img: "https://http2.mlstatic.com/D_NQ_NP_988424-MLA46526341782_062021-O.jpg",
         title: "Tokio Ghoul",
         desc: "Historia donde Kaneki folla con Touka-chan al final",
-        bg: "fff",
+        bg: "CAE4E2",
     },
     {
         id: 3,
         img: "https://http2.mlstatic.com/D_NQ_NP_780153-MLA46604101698_072021-O.webp",
         title: "Akame Ga Kill",
         desc: "Un buen anime que me encanta su Waifu de pelo azul uwu",
-        bg: "fff",
+        bg: "CAE4E2",
     },
 ];
 
@@ -39,3 +39,37 @@ export const categories = [
     //   title: "LIGHT JACKETS",
     // },
   ];
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://tienda-m.com.ar/wp-content/uploads/2021/10/tokyo-revengers-wallpaper.jpg",
+    },
+    {
+      id:2,
+      img:"https://tienda-m.com.ar/wp-content/uploads/2021/10/My-Neighbor-Totoro-Background-_-Fanart.jpg",
+    },
+    {
+      id:3,
+      img:"https://d3ugyf2ht6aenh.cloudfront.net/stores/001/015/914/products/136406985_1320316331684808_585705191401654665_n1-879ab7eae0295fcea416293054934424-1024-1024.jpg",
+    },
+    {
+      id:4,
+      img:"https://http2.mlstatic.com/D_NQ_NP_617948-MLA43813305101_102020-O.jpg",
+    },
+    {
+      id:5,
+      img:"http://d3ugyf2ht6aenh.cloudfront.net/stores/001/281/199/products/medio-111-e8f87027947a3dc47416094235612070-640-0.jpg",
+    },
+    {
+      id:6,
+      img:"http://d3ugyf2ht6aenh.cloudfront.net/stores/001/015/914/products/136406985_1320316331684808_585705191401654665_n1-879ab7eae0295fcea416293054934424-640-0.jpg",
+    },
+    {
+      id:7,
+      img:"https://http2.mlstatic.com/D_NQ_NP_837720-MLA43813214677_102020-O.webp",
+    },
+    {
+      id:8,
+      img:"http://d3ugyf2ht6aenh.cloudfront.net/stores/001/015/914/products/hunter-x-hunter-rage-gon-neferpitou-11-ed79d00deaf2fe525a16172145338432-640-0.jpg",
+    },
+  ]
