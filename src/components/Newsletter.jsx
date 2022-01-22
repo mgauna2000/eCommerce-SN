9de8e4976e97405d61e835e425d5aea5d88da39c
Martin <martingauna2000@gmail.com>
2022-01-21 21:54:57 -0300

    terminando newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,32 +2,51 @@ import { Send } from '@material-ui/icons';
 import styled from 'styled-components';
 
 const Container = styled.div`
-
+    height: 40vh;
+    background-color: #CAE4E2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 `;
 const Title = styled.h1`
-    
+    font-size: 70px;
+    margin-bottom: 20px;
 `;
 const Description = styled.div`
-    
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
 `;
 const InputContainer = styled.div`
-    
+    width: 50%;
+    height: 40px;
+    background-color: #fff;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
 `;
 const Input = styled.input`
-    
+    border: none;
+    flex: 8;
+    padding-left: 20px;
 `;
 const Button = styled.button`
-    
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: #fff;
+    cursor: pointer;
 `;
 
 
 const Newsletter = () => {
   return (
       <Container>
-          <Title></Title>
-          <Description></Description>
+          <Title>Novedades</Title>
+          <Description>Mira las novedades de tus prductos favoritos</Description>
           <InputContainer>
-            <Input/>
+            <Input placeholder='Su Email' />
             <Button>
                 <Send />
             </Button>
