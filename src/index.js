aa87ba2b2054510f3d143b16578cf02e3fd15557
Martin <martingauna2000@gmail.com>
2022-01-23 21:46:44 -0300

    añadiendo productlist

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import ProductList from './pages/ProductList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductList />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+
+const Container = styled.div`
+
+`;
+
+const ProductList = () => {
+  return (
+      <Container>
+        <Navbar/>
+      </Container>
+  )
+};
+
+export default ProductList;
