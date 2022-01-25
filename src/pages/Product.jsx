6169eaeec9242e2a409655ead8cb2f9f010033eb
Martin <martingauna2000@gmail.com>
2022-01-24 23:16:46 -0300

    update product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar"
 import Announcement from "../components/Announcement"
 import Newsletter from "../components/Newsletter"
 import Footer from "../components/Footer"
+import { Add, Remove } from "@material-ui/icons";
 
 const Container = styled.div`
 
@@ -33,6 +34,45 @@ const Price = styled.span`
   font-weight: 100;
   font-size: 40px;
 `;
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+
+`;
+const FilterSizeOption = styled.option`
+  
+`;
+const AddContainer = styled.div`
+  
+`;
+const AmountContainer = styled.div`
+  
+`;
+const Amount = styled.span`
+`;
+const Button = styled.button`
+  
+`;
 
 const Product = () => {
   return (
@@ -55,12 +95,25 @@ const Product = () => {
                   <FilterColor color="gray" />
                 </Filter>
                 <Filter>
-                  <FilterTitle>Color</FilterTitle>
-                  <FilterColor color="black" />
-                  <FilterColor color="darkblue" />
-                  <FilterColor color="gray" />
+                  <FilterTitle>Size</FilterTitle>
+                  <FilterSize>
+                  <FilterSizeOption>XL</FilterSizeOption>
+                  <FilterSizeOption>S</FilterSizeOption>
+                  <FilterSizeOption>M</FilterSizeOption>
+                  <FilterSizeOption>L</FilterSizeOption>
+                  <FilterSizeOption>XL</FilterSizeOption>
+                  <FilterSizeOption>XXL</FilterSizeOption>
+                  </FilterSize>
                 </Filter>
               </FilterContainer>
+              <AddContainer>
+                <AmountContainer>
+                  <Remove/>
+                  <Amount>1</Amount>
+                  <Add/>
+                </AmountContainer>
+                <Button>Añadir al carrito</Button>
+              </AddContainer>
             </InfoContainer>
           </Wrapper>
           <Newsletter/>
